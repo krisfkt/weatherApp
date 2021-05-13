46cfa1cdea1c8f346755995aedb5779018ac0269
krisfkt <krisfkt@gmail.com>
2021-05-13 11:20:12 +0800

    Initial commit

---
+++ app/src/main/java/student/weatherapp/MainActivity.java
@@ -0,0 +1,208 @@
+package student.weatherapp;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.JsonHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
+
+import org.json.JSONObject;
+
+import cz.msebera.android.httpclient.Header;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    final String APP_ID = "858c8d05373e0a12d059a64cdaba1ba1";
+    final String WEATHER_URL = "https://api.openweathermap.org/data/2.5/weather";
+
+    final long MIN_TIME = 1000;
+    final float MIN_DISTANCE = 1000;
+    final int REQUEST_CODE = 101;
+
+    String Location_Provider = LocationManager.GPS_PROVIDER;
+    TextView CITYNAME, WEATHERSTATE, TEMPERATURE, HUMIDITY, WIND, TEMP_MAX, TEMP_MIN;
+    ImageView WEATHERICON;
+    RelativeLayout CITYFINDER;
+    LocationManager LOCATIONMANAGER;
+    LocationListener LOCATIONLISTENER;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        WEATHERSTATE = findViewById(R.id.condition);
+        TEMPERATURE = findViewById(R.id.temperature);
+        WEATHERICON = findViewById(R.id.weatherIcon);
+        CITYFINDER = findViewById(R.id.relocate);
+        CITYNAME = findViewById(R.id.cityName);
+        HUMIDITY = findViewById(R.id.humidity);
+        WIND = findViewById(R.id.wind);
+        TEMP_MAX = findViewById(R.id.temp_max);
+        TEMP_MIN = findViewById(R.id.temp_min);
+
+        CITYFINDER.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, city.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Intent mIntent=getIntent();
+        String city= mIntent.getStringExtra("City");
+        if(city!=null)
+        {
+            getWeatherForNewCity(city);
+        }
+        else
+        {
+            getWeatherForCurrentLocation();
+        }
+    }
+
+    private void getWeatherForNewCity(String city)
+    {
+        RequestParams params=new RequestParams();
+        params.put("q",city);
+        params.put("appid",APP_ID);
+        networkingStuff(params);
+    }
+
+    private void getWeatherForCurrentLocation() {
+
+        LOCATIONMANAGER = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        LOCATIONLISTENER = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+
+                String Latitude = String.valueOf(location.getLatitude());
+                String Longitude = String.valueOf(location.getLongitude());
+
+                RequestParams params =new RequestParams();
+                params.put("lat" ,Latitude);
+                params.put("lon",Longitude);
+                params.put("appid",APP_ID);
+                networkingStuff(params);
+
+
+            }
+
+        };
+
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.ACCESS_FINE_LOCATION},REQUEST_CODE);
+            return;
+        }
+        LOCATIONMANAGER.requestLocationUpdates(Location_Provider, MIN_TIME, MIN_DISTANCE, LOCATIONLISTENER);
+
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+
+        if(requestCode==REQUEST_CODE)
+        {
+            if(grantResults.length>0 && grantResults[0]==PackageManager.PERMISSION_GRANTED)
+            {
+                Toast.makeText(MainActivity.this,"Location recognized",Toast.LENGTH_SHORT).show();
+                getWeatherForCurrentLocation();
+            }
+            else
+            {
+                //user denied the permission
+            }
+        }
+
+
+    }
+
+
+
+    private  void networkingStuff(RequestParams params)
+    {
+        AsyncHttpClient client = new AsyncHttpClient();
+        client.get(WEATHER_URL,params,new JsonHttpResponseHandler()
+        {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+
+                Toast.makeText(MainActivity.this,"City Weather Updated",Toast.LENGTH_SHORT).show();
+
+                weatherData weatherZ=weatherData.fromJson(response);
+                updateUI(weatherZ);
+
+
+                // super.onSuccess(statusCode, headers, response);
+            }
+
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONObject errorResponse) {
+                //super.onFailure(statusCode, headers, throwable, errorResponse);
+            }
+        });
+
+
+
+    }
+
+    private  void updateUI(weatherData weather){
+
+
+        TEMPERATURE.setText(weather.getzTemperature());
+        CITYNAME.setText(weather.getzCity());
+        WEATHERSTATE.setText(weather.getzWeatherType());
+        int resourceID=getResources().getIdentifier(weather.getzIcon(),"drawable",getPackageName());
+        WEATHERICON.setImageResource(resourceID);
+        HUMIDITY.setText(weather.getzHumidity());
+        WIND.setText(weather.getzWind());
+        TEMP_MAX.setText(weather.getzTemp_Max());
+        TEMP_MIN.setText(weather.getzTemp_Min());
+
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if(LOCATIONMANAGER!=null)
+        {
+            LOCATIONMANAGER.removeUpdates(LOCATIONLISTENER);
+        }
+    }
+}
---
+++ app/src/main/java/student/weatherapp/weatherData.java
@@ -0,0 +1,114 @@
+package student.weatherapp;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class weatherData {
+
+    private String zTemperature,zIcon,zCity,zWeatherType,zHumidity,zWind,zTemp_Max,zTemp_Min;
+    private int zCondition;
+
+    public static weatherData fromJson(JSONObject jsonObject)
+    {
+
+        try
+        {
+            weatherData weatherZ =new weatherData();
+            weatherZ.zCity=jsonObject.getString("name");
+            weatherZ.zWeatherType=jsonObject.getJSONArray("weather").getJSONObject(0).getString("main");
+            int humidity=jsonObject.getJSONObject("main").getInt("humidity");
+            weatherZ.zHumidity=Integer.toString(humidity);
+            weatherZ.zCondition=jsonObject.getJSONArray("weather").getJSONObject(0).getInt("id");
+            double windResult=jsonObject.getJSONObject("wind").getDouble("speed")*60*60/1000;
+            int roundedWindValue=(int)Math.rint(windResult);
+            weatherZ.zWind=Integer.toString(roundedWindValue);
+            double tempResult=jsonObject.getJSONObject("main").getDouble("temp")-273.15;
+            int roundedValue=(int)Math.rint(tempResult);
+            weatherZ.zTemperature=Integer.toString(roundedValue);
+            weatherZ.zIcon=updateWeatherIcon(weatherZ.zCondition);
+            double tempMaxResult=jsonObject.getJSONObject("main").getDouble("temp_max")-273.15;
+            int roundedMaxValue=(int)Math.rint(tempMaxResult);
+            weatherZ.zTemp_Max=Integer.toString(roundedMaxValue);
+            double tempMinResult=jsonObject.getJSONObject("main").getDouble("temp_min")-273.15;
+            int roundedMinValue=(int)Math.rint(tempMinResult);
+            weatherZ.zTemp_Min=Integer.toString(roundedMinValue);
+
+
+            return weatherZ;
+        }
+
+
+        catch (JSONException e) {
+            e.printStackTrace();
+            return null;
+        }
+
+
+    }
+
+
+    private static String updateWeatherIcon(int condition)
+    {
+        if(condition>=200 && condition<=299)
+        {
+            return "thunderstrom";
+        }
+        else if(condition>=300 && condition<=499)
+        {
+            return "lightrain";
+        }
+        else if(condition>=500 && condition<=599)
+        {
+            return "rain";
+        }
+        else  if(condition>=600 && condition<=699)
+        {
+            return "cold";
+        }
+        else if(condition>=700 && condition<=799)
+        {
+            return "fog";
+        }
+        else if(condition==800)
+        {
+            return "sunny";
+        }
+        else if(condition>=801 && condition<=802)
+        {
+            return "cloudy";
+        }
+        else if(condition>=803 && condition<=804)
+        {
+            return "overcast";
+        }
+
+        return "nothing";
+
+
+    }
+
+    public String getzTemperature() {
+        return zTemperature+"°C";
+    }
+
+    public String getzIcon() {
+        return zIcon;
+    }
+
+    public String getzCity() {
+        return zCity;
+    }
+
+    public String getzWeatherType() {
+        return zWeatherType;
+    }
+
+    public String getzHumidity() {return "Humidity:"+zHumidity+"%";}
+
+    public String getzWind() {return "Wind: "+zWind+" km/h";}
+
+    public String getzTemp_Max() {return "H:"+zTemp_Max+"°";}
+
+    public String getzTemp_Min() {return "L:"+zTemp_Min+"°";}
+
+}
